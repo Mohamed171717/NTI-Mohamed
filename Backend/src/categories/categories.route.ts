b5c85d946dede82a7b4874f2b26f79e28592e88a
Mohamed <fouademail@gmail.com>
2024-12-12 09:23:06 +0200

    update1

--- Backend/src/categories/categories.route.ts
+++ Backend/src/categories/categories.route.ts
@@ -11,12 +11,12 @@ categoriesRouter.use( "/:categoryId/subcategories", subcategoriesRouter )
 
 categoriesRouter.route('/')
 .get(categoriesService.getAllCategories)
-.post(categoriesValidation.createOne,categoriesService.createCategory)
+.post(categoriesValidation.createOne, categoriesService.createCategory)
 
 categoriesRouter.route('/:id')
-.get(categoriesValidation.getOne,categoriesService.getCategory)
-.put(categoriesValidation.updateOne,categoriesService.updateCategory)
-.delete(categoriesValidation.deleteOne,categoriesService.deleteCategory)
+.get(categoriesValidation.getOne, categoriesService.getCategory)
+.put(categoriesValidation.updateOne, categoriesService.updateCategory)
+.delete(categoriesValidation.deleteOne, categoriesService.deleteCategory)
 
 
 export default categoriesRouter;
